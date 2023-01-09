bb13f8c6e69a92862f52667822c960bcb868f9ba
MarioP-Me <119905639+MarioP-Me@users.noreply.github.com>
2023-01-09 11:38:23 -0600

    p7

--- js/apple.js
+++ js/apple.js
@@ -2,7 +2,7 @@ var rand = Math.floor((Math.random() * 3) + 1);
 alert(rand)
 if(rand == 2){
 	alert("Apple")
-	document.body.style.backgroundImage = "url(pics/Anthro_Coat_Test_Centered.png)";
+	document.body.style.backgroundImage = "url('pics/Anthro_Coat_Test_Centered.png')";
 }
 
 function PreviewLetter() {
