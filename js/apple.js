d5094fe7a659b768f41201902400091742d40ce7
MarioP-Me <119905639+MarioP-Me@users.noreply.github.com>
2023-01-09 11:57:39 -0600

    Easter Egg Scrap

--- js/apple.js
+++ js/apple.js
@@ -1,12 +1,3 @@
-var rand = Math.floor((Math.random() * 3) + 1);
-alert(rand)
-if(rand == 2){
-	alert("Apple")
-	document.body.style.backgroundImage = "url('https://mariop-me.github.io/pics/Anthro_Coat_Test_Centered.png')";
-}else{
-	document.body.style.backgroundImage = "url('https://mariop-me.github.io/pics/Background.jpg')"
-}
-
 function PreviewLetter() {
 	alert(document.getElementById("LetterOut").value);
 	var Letter = document.getElementById("LetterOut").value;
