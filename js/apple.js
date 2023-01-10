30a6f76dc62e20020c413e25b3c8ec0a21ebbe93
MarioP-Me <119905639+MarioP-Me@users.noreply.github.com>
2023-01-10 12:05:45 -0600

    Background Test 1

--- js/apple.js
+++ js/apple.js
@@ -1,3 +1,5 @@
+document.body.style.backgroundImage = "url('https://mariop-me.github.io/pics/Background.jpg')";
+
 function PreviewLetter() {
 	alert(document.getElementById("LetterOut").value);
 	var Letter = document.getElementById("LetterOut").value;
