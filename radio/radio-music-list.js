1f1cdcb2356da81d6baedf07e9ffbbf00992b32b
Blue <119905639+2MuchBlue@users.noreply.github.com>
2026-02-26 16:37:25 -0600

    added crafting page (wip)

--- radio/radio-music-list.js
+++ radio/radio-music-list.js
@@ -12,5 +12,6 @@ class RadioSong {
 let songs = [
     new RadioSong("https://2muchblue.github.io/Audio/OutsideTheFoxElementSongFull.wav", "Outside the Fox Main Theme", "Blue", 37),
     new RadioSong("https://2muchblue.github.io/Audio/Crystal.wav", "Crystal Waves", "Blue", 96),
-    new RadioSong("https://2muchblue.github.io/Audio/Room2DrumAndBulletGame.wav", "Boss 2 from Drum & Bass (a game", "Blue)", 36)
+    new RadioSong("https://2muchblue.github.io/Audio/Room2DrumAndBulletGame.wav", "Boss 2 from Drum & Bass (a game", "Blue)", 36),
+    new RadioSong("https://2muchblue.github.io/Audio/WarmDusk.wav", "Warm Dusk", "Blue", 42)
 ];
